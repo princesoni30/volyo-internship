21b66b2333bfaffca8a1f01d6a7401b81a9a5c97
princesoni30 <babulal7412@gmail.com>
2026-04-02 11:41:14 +0530

    dono ki age ek hai

---
+++ marriage.py
@@ -0,0 +1,10 @@
+male_age = int(input("Enter male age: "))
+female_age = int(input("Enter female age: "))
+
+if male_age >= 21 and female_age >= 18:
+    print("Eligible for Marriage ✅")
+else:
+    if male_age < 21:
+        print("Sorry! Male not eligible ❌")
+    if female_age < 18:
+        print("Sorry! Female not eligible ❌")
