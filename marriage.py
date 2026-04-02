dfb60b107aee2d3b951a2767318ee93b9af7fb53
Prince soni <babulal7412@gmail.com>
2026-04-02 12:19:10 +0530

    Remove emoji from eligibility messages

--- marriage.py
+++ marriage.py
@@ -2,9 +2,9 @@ male_age = int(input("Enter male age: "))
 female_age = int(input("Enter female age: "))
 
 if male_age >= 21 and female_age >= 18:
-    print("Eligible for Marriage ✅")
+    print("Eligible for Marriage ")
 else:
     if male_age < 21:
-        print("Sorry! Male not eligible ❌")
+        print("Sorry! Male not eligible ")
     if female_age < 18:
-        print("Sorry! Female not eligible ❌")
+        print("Sorry! Female not eligible ")
